912d2fbadc66447d9e91391ba8ad7a76b5e15336
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-04-21 23:09:09 +0330

    add delete api

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ const server = http.createServer((req, res) => {
             res.write(JSON.stringify(data.users))
             res.end()
         })
-    }
-    else if (req.method === 'GET' && req.url === '/api/books') {
+    } else if (req.method === 'GET' && req.url === '/api/books') {
         fs.readFile('db.json', (err, db) => {
             if (err) {
                 throw err
@@ -28,25 +27,24 @@ const server = http.createServer((req, res) => {
             res.write(JSON.stringify(data.books))
             res.end()
         })
-    }
-    else if (req.method === 'DELETE') {
+    } else if (req.method === 'DELETE' && req.url.startsWith("/api/books")) {
         const parsedUrl = url.parse(req.url, true)
         const bookId = parsedUrl.query.id
-        console.log(db.books.length)
-        // if (db.books.length <=0) {
-        //     res.writeHead(404, {'content-type': 'application/json'})
-        //     res.write(JSON.stringify({message: "No Book Here"}))
-        //     res.end()
-        // }
         const newBooks = db.books.filter(book => book.id != bookId)
-        fs.writeFile('db.json', JSON.stringify({...db, books: newBooks}), (err) => {
-            if (err) {
-                throw err
-            }
-            res.writeHead(200, {'content-type': 'application/json'})
-            res.write(JSON.stringify({message: "Book Removed Successfully"}))
+
+        if (newBooks.length === db.books.length) {
+            res.writeHead(401, {'content-type': 'application/json'})
+            res.write(JSON.stringify({message: "Book Not Found"}))
             res.end()
-        })
+        } else {
+            fs.writeFile('db.json', JSON.stringify({...db, books: newBooks}), (err) => {
+                if (err) throw err
+                res.writeHead(200, {'content-type': 'application/json'})
+                res.write(JSON.stringify({message: "Book Removed Successfully"}))
+                res.end()
+            })
+        }
+
     }
 })
 
