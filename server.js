993aa1bff648d0c0e335316a31c685a0fd403a96
Mohammad Behnam <muhammad.behnam@yahoo.com>
2024-04-21 20:24:24 +0330

    add delete methods

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url")
+const db = require('./db.json')
+
 
 const port = 4000;
 
@@ -24,6 +27,19 @@ const server = http.createServer((req, res) => {
             res.write(JSON.stringify(data.books))
             res.end()
         })
+    } else if (req.method === 'DELETE') {
+        const parsedUrl = url.parse(req.url, true)
+        const bookId = parsedUrl.query.id
+        const newBooks = db.books.filter(book => book.id != bookId)
+        fs.writeFile('db.json', JSON.stringify({...db, books: newBooks}), (err) => {
+            if (err) {
+                throw err
+            }
+            res.writeHead(200, {'content-type': 'application/json'})
+            res.write(JSON.stringify({message: "Book Removed Successfully"}))
+            res.end()
+        })
+        res.end()
     }
 })
 
